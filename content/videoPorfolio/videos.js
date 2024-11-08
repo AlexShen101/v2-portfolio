1ac0a1ac5b983cdce242784caeaae9110876ca83
alexander shen <alexander.shen@gmail.com>
2024-11-08 00:10:09 -0500

    added a new short

--- content/videoPorfolio/videos.js
+++ content/videoPorfolio/videos.js
@@ -1,5 +1,6 @@
 export const videos = [
   { id: '9668EqHIyXw', title: 'Demo Reel', type: 'demoReel', date: '2024-10-10' },
   { id: 'bRgz35bR6Ck', title: 'Squash Stretch Text', type: 'short' },
+  { id: 'EQ_n3s9zy6A', title: 'Morning Star', type: 'short' },
   // Add more videos as needed
 ];
